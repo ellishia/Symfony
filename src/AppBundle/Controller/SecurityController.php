d1aa08e87fc75c3b5eb35f46763afd2c1a3d641e
ellishia <ellishia@gmail.com>
2017-10-03 13:43:27 +0200

    Update SecurityController.php
    Functions for new Artista and Modelo in Security controller

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -142,16 +142,41 @@ class SecurityController extends Controller
             return $this->redirect($this->generateUrl('addObra', array('request' => $request)));
         }
         
-        return $this->render('default/createArtista.html.twig', array('form' => $form->createView(), 'obra' => $obra ));
+        return $this->render('default/createModelo.html.twig', array('form' => $form->createView(), 'obra' => $obra ));
     }
     
-    
-    
-    
-    
-    
-    
-    
+     /**
+     * @Route("/addUser", name="addUser")
+     */
+    public function newUserAction(Request $request)
+    {
+        // 1) build the form
+        $user = new Usuario();
+        $form = $this->createForm(UsuarioType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('');
+        }
+
+        return $this->render('default/register.html.twig', array('form' => $form->createView())
+        );
+    }    
     
     public function authenticateUser(Usuario $user)
     {
